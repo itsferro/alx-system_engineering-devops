52d33b36f3d809731f867c342e8407ee15a5ab9a
itsferro <feras.mamon.alzaidi@gmail.com>
2024-03-23 16:45:31 -0700

    Task number 1 attempt number 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #install flask from pip3.
 #Version 2.1.0
 
-exec { 'flask installation':
-  command => 'pip install flask==2.1.0',
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
