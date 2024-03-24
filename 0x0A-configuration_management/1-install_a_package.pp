d56d45d87bf823a5c415c11ff649201c7d54635a
itsferro <feras.mamon.alzaidi@gmail.com>
2024-03-23 19:49:17 -0700

    Task number 1 atempt number 9

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #install flask from pip3.
 
-package {'flask':
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package {'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
