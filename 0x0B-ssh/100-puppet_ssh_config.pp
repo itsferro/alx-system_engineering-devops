5213a57278d3a1fd4ddecc4ffc5a35af866adb30
itsferro <feras.mamon.alzaidi@gmail.com>
2024-03-25 00:10:03 -0700

    Task number 4 attempt number 3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #so that you can connect to a server without typing a password.
 
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
+  path  => '~/.ssh/config',
   line  => ' IdentityFile ~/.ssh/school',
   match => '^Host *',
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/config',
+  path  => '~/.ssh/config',
   line  => ' PasswordAuthentication no',
   match => '^Host *',
 }
