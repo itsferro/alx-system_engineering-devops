46f82295376a85e628b0c399865d9b995af1d761
itsferro <feras.mamon.alzaidi@gmail.com>
2024-03-25 01:20:04 -0700

    Task number 4 attempt number 8

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,31 @@
 #so that you can connect to a server without typing a password.
 include stdlib
 
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-  mode   => 0600,
-}
+# file { '/etc/ssh/ssh_config':
 
-file_line { 'The host configuration':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => 'Host *',
-  match              => 'Host *',
-  replace            => true,
-  append_on_no_match => true,
-}
+# ensure => present,
 
+# mode   => 0600,
+
+# }
+
+# 
+
+# file_line { 'The host configuration':
+
+# ensure             => present,
+
+# path               => '/etc/ssh/ssh_config',
+
+# line               => 'Host *',
+
+# match              => 'Host *',
+
+# replace            => true,
+
+# append_on_no_match => true,
+
+# }
 file_line { 'Declare identity file':
   ensure             => present,
   path               => '/etc/ssh/ssh_config',
