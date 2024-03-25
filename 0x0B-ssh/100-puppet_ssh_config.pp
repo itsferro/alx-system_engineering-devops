dff6b91ceaa5aececc7b5fb62e022f68f2af4a9a
itsferro <feras.mamon.alzaidi@gmail.com>
2024-03-25 01:04:09 -0700

    Task number 2 and number 4 revesion

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,33 @@
 #so that you can connect to a server without typing a password.
 include stdlib
 
+file { '/root/.ssh/config':
+  ensure => present,
+}
+
+file_line { 'The host configuration':
+  ensure             => present,
+  path               => '/root/.ssh/config',
+  line               => 'Host *',
+  match              => 'Host *',
+  replace            => true,
+  append_on_no_match => true,
+}
+
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
-  line  => ' IdentityFile ~/.ssh/school',
-  match => '^Host *',
+  ensure             => present,
+  path               => '/root/.ssh/config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '    IdentityFile ',
+  replace            => true,
+  append_on_no_match => true,
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/config',
-  line  => ' PasswordAuthentication no',
-  match => '^Host *',
+  ensure             => present,
+  path               => '/root/.ssh/config',
+  line               => '    PasswordAuthentication no',
+  match              => '    PasswordAuthentication ',
+  replace            => true,
+  append_on_no_match => true,
 }
