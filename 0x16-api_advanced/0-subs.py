75679f8924ffa0a16ad8192863f1920e59ab556d
itsferro <feras.mamon.alzaidi@gmail.com>
2024-05-08 04:17:46 +0200

    Task number 0 attempt number 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,8 +19,10 @@ def number_of_subscribers(subreddit):
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
+            if response.json().data:
+                return response.json().data["data"]["subscribers"]
+            else:
+                return 0
         else:
             return 0
     except requests.exceptions.RequestException:
