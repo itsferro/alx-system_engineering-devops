8e9803f8597eb53e7502b17a9b8afb45902b2200
itsferro <feras.mamon.alzaidi@gmail.com>
2024-05-09 23:26:27 +0200

    Task number 1 attempt number 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+import sys
+
+
+def print_10(titles):
+    """
+    loops through the dict and prints it
+    """
+    for title in titles:
+        print(title)
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}.json"
+    headers = {"User-Agent": "Mozilla/5.0", "raw_json": "1"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            titles = []
+            for i in range(10):
+                try:
+                    titles.append(data["data"]["children"][i]["data"]["title"])
+                except Exception:
+                    if len(titles):
+                        pass
+                    else:
+                        print(None)
+                        sys.exit()
+            print_10(titles)
+            sys.exit()
+        else:
+            print(None)
+    except requests.exceptions.RequestException:
+        print(None)
