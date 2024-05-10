84ee250794f8994c210a0ba1d8e8f4f71276aafe
itsferro <feras.mamon.alzaidi@gmail.com>
2024-05-10 04:47:11 +0200

    Task number 1 attempt number 101

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -20,25 +20,15 @@ def top_ten(subreddit):
     queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0", "raw_json": "1"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            titles = []
-            for i in range(10):
-                try:
-                    titles.append(data["data"]["children"][i]["data"]["title"])
-                except Exception:
-                    if len(titles):
-                        pass
-                    else:
-                        print(None)
-                        sys.exit()
-            print_10(titles)
-            sys.exit()
+            for child in data["data"]["children"]:
+                print(child["data"]["title"])
         else:
             print(None)
     except requests.exceptions.RequestException:
