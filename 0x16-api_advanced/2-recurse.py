825df68d58706dd7457f35aa669186d15b15db5d
itsferro <feras.mamon.alzaidi@gmail.com>
2024-05-10 03:04:45 +0200

    Task number 2 attempt number 1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,39 +7,31 @@ import requests
 import sys
 
 
-def print_10(titles):
-    """
-    loops through the dict and prints it
-    """
-    for title in titles:
-        print(title)
-
-
-def top_ten(subreddit):
+def recurse(subreddit, hot_list=[], after=None):
     """
     queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}.json?limit=100&after={after}"
     headers = {"User-Agent": "Mozilla/5.0", "raw_json": "1"}
 
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            titles = []
-            for i in range(10):
+            for i in range(100):
                 try:
-                    titles.append(data["data"]["children"][i]["data"]["title"])
+                    hot_list.append(data["data"]["children"][i]["data"]["title"])
                 except Exception:
-                    if len(titles):
+                    if len(hot_list):
                         pass
                     else:
-                        print(None)
-                        sys.exit()
-            print_10(titles)
-            sys.exit()
+                        return None
+            if data["data"]["after"]:
+                return recurse(subreddit, hot_list, data["data"]["after"])
+            else:
+                return hot_list
         else:
-            print(None)
+            return None
     except requests.exceptions.RequestException:
-        print(None)
+        return None
